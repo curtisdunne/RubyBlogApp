d255548d968d6102e13a4dd83ed06d9e33990409
Curtis Dunne <curtdunne@gmail.com>
2019-03-05 17:04:56 -0700

    removed the blog crap

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#home'
   get 'about', to: 'pages#about'
-  get 'blog/pages', to: 'blog#pages'
 
-  # resources gives us all CRUD functions as well as Show   
+  # resources gives us all CRUD functions as well as Show
   resources :articles
 end
