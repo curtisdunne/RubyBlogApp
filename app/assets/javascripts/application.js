b5caa486412166e8e7115715e3678f4d3c106c62
Curtis Dunne <curtdunne@gmail.com>
2019-03-07 10:01:45 -0700

    styled the home page with bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,7 @@
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
-//= require_tree .
-//= require rails-ujs
-//= require jquery
-//= require bootstrap-sprockets
+//= require jquery3
+//= require popper
+// = require_tree .
+//= require bootstrap
