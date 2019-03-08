0660001d813bf94165ed4bb78908e4034ba9bd92
Curtis Dunne <curtdunne@gmail.com>
2019-03-08 13:39:03 -0700

    created users controller and model; updated routes to allow adding new users

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
 
   # resources gives us all CRUD functions as well as Show
   resources :articles
+
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
